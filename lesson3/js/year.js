53c5e964d5f77bb27cb13afe74cb22a41801a037
karrassphiri <phi21020@byui.edu>
2022-10-05 04:41:55 +0200

    week 3 assignment commit with feedback changes

--- lesson3/js/year.js
+++ lesson3/js/year.js
@@ -1,2 +1,6 @@
+const year = {year:'numeric'};
+document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', year);
+
 const options = {day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', options);
+
