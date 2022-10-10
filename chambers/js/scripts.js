5a2585ebcb883c4c859a3e1578f8e459aa44e742
karrassphiri <phi21020@byui.edu>
2022-10-10 04:52:55 +0200

    changes in colors

--- chambers/js/scripts.js
+++ chambers/js/scripts.js
@@ -6,6 +6,28 @@ function toggleMenu(){
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
+//script for header date
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date1");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
+
+
+
 //script for year
 const year = {year:'numeric'};
 document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', year);
