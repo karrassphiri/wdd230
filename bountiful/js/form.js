4dec217b20252c7eeffdb1fd3705d9ab96012ff6
karrassphiri <phi21020@byui.edu>
2022-12-14 12:00:41 +0200

    fresh page edited

--- bountiful/js/form.js
+++ bountiful/js/form.js
@@ -1,28 +1,60 @@
-/*const form = document.getElementById('form');
+/*form.addEventListener('submit', function(e){
+  e.preventDefault()*/
 
-form.addEventListener('submit', function(e){
-  e.preventDefault();
-
-  const payload = new FormData(form);
-
-  console.log([...payload]);
-  fetch("https://fruityvice.com/api/fruit", {
-    method: "POST",
-    body: data,
-  })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-})*/
 function check() {
+
   document.getElementById('firstname').innerHTML = document.form.name.value;
   document.getElementById('phonenum').innerHTML = document.form.phone.value;
   document.getElementById('emailadd').innerHTML = document.form.email.value;
-  document.getElementById('fr1').innerHTML = document.form.fruit1.value;
-  document.getElementById('fr2').innerHTML = document.form.fruit2.value;
-  document.getElementById('fr3').innerHTML = document.form.fruit3.value;
   document.getElementById('instruct').innerHTML = document.form.user_message.value;
+
+  const options = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
+  document.getElementById('formdate').textContent = new Date().toLocaleDateString('en-US', options);
+  document.getElementById('thanks').innerHTML = 'We will make your juice in due time!'
 }
 
+/****Script for displaying fruits nutritions******/
+const reqtURL = ('https://karrassphiri.github.io/wdd230/bountiful/json/fruits.json')
+const gridbutton = document.querySelector("#grid");
+const display = document.querySelector("#cards");
 
+function fruitDetails(info, type) {
+  let data = info.fruits;
+  data.forEach((fruit) => {
+    let card = document.createElement("section");
+    let p = document.createElement("p");
+    let p2 = document.createElement("p2");
+
+    card.setAttribute("class", "section");
+    p.innerHTML = `${fruit.name}`;
+    p2.innerHTML = `${fruit.nutritions}`;
+
+    card.appendChild(p);
+    card.appendChild(p2);
+    
+
+    display.classList.add(type);
+    display.append(card);
+  });
+}
+
+async function getFruits(type) {
+  let response = await fetch(reqtURL);
+  if (response.ok) {
+    let data = await response.json();
+    buildBusinessCards(data, type);
+  } else {
+    throw Error(response.statusText);
+  }
+}
+getFruits("grid");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+  
+});
 
